65aec9a2e8360d029fc716ba373fc1c1d9d8f173
NAgnekil <nathalie.agnekil@gmail.com>
2025-02-06 15:27:15 +0100

    implemented filtering on cold sowing

--- src/components/QuerySection.vue
+++ src/components/QuerySection.vue
@@ -1,46 +1,58 @@
 <script setup>
-import { ref, watch, defineProps, defineEmits } from "vue";
-import ColorPicker from "./ColorPicker.vue";
-import CategoryPicker from "./CategoryPicker.vue";
+  import { ref, watch, defineProps, defineEmits } from 'vue'
+  import ColorPicker from './ColorPicker.vue'
+  import CategoryPicker from './CategoryPicker.vue'
 
-const props = defineProps({
-  categories: Array,
-  colors: Array,
-  cultivationTypes: Array,
-});
+  const props = defineProps({
+    categories: Array,
+    colors: Array,
+    cultivationTypes: Array,
+    coldPlantingTypes: Array
+  })
 
-const emit = defineEmits([
-  "update:searchName",
-  "update:chosenCategory",
-  "update:chosenColor",
-  "update:chosenCultivationType",
-]);
+  const emit = defineEmits([
+    'update:searchName',
+    'update:chosenCategory',
+    'update:chosenColor',
+    'update:chosenCultivationType',
+    'update:chosenColdPlantingType'
+  ])
 
-const searchName = ref("");
-const chosenCategory = ref([]);
-const chosenCultivationType = ref("all");
-const chosenColor = ref([]);
-const cultivationTypeNames = {
-  All: "Alla",
-  SowIndoors: "Förodlas",
-  SowOutdoors: "Direktsås",
-};
+  const searchName = ref('')
+  const chosenCategory = ref([])
+  const chosenCultivationType = ref('allCultivationTypes')
+  const chosenColdPlantingType = ref('allColdPlantingTypes')
+  const chosenColor = ref([])
+  const cultivationTypeNames = {
+    allCultivationTypes: 'Alla',
+    SowIndoors: 'Förodlas',
+    SowOutdoors: 'Direktsås'
+  }
+  const coldPlantingNames = {
+    allColdPlantingTypes: 'Alla',
+    Yes: 'Ja',
+    No: 'Nej'
+  }
 
-watch(searchName, (newValue) => {
-  emit("update:searchName", newValue);
-});
+  watch(searchName, (newValue) => {
+    emit('update:searchName', newValue)
+  })
 
-watch(chosenColor, (newValue) => {
-  emit("update:chosenColor", newValue);
-});
+  watch(chosenColor, (newValue) => {
+    emit('update:chosenColor', newValue)
+  })
 
-watch(chosenCategory, (newValue) => {
-  emit("update:chosenCategory", newValue);
-});
+  watch(chosenCategory, (newValue) => {
+    emit('update:chosenCategory', newValue)
+  })
 
-watch(chosenCultivationType, (newValue) => {
-  emit("update:chosenCultivationType", newValue);
-});
+  watch(chosenCultivationType, (newValue) => {
+    emit('update:chosenCultivationType', newValue)
+  })
+
+  watch(chosenColdPlantingType, (newValue) => {
+    emit('update:chosenColdPlantingType', newValue)
+  })
 </script>
 
 <template>
@@ -64,23 +76,57 @@ watch(chosenCultivationType, (newValue) => {
         <input
           class="radio-button"
           type="radio"
-          id="all"
-          value="all"
+          id="allCultivationTypes"
+          value="allCultivationTypes"
           v-model="chosenCultivationType"
           name="cultivationType"
         />
-        <label for="all">Alla</label><span class="checkmark"></span>
+        <label for="allCultivationTypes">Alla</label
+        ><span class="checkmark"></span>
       </div>
-      <div class="radios" v-for="type in cultivationTypes" :key="type">
+      <div
+        class="radios"
+        v-for="cultivationType in cultivationTypes"
+        :key="cultivationType"
+      >
+        <input
+          class="radio-button"
+          type="radio"
+          :id="cultivationType"
+          :value="cultivationType"
+          v-model="chosenCultivationType"
+          name="cultivationType"
+        />
+        <label :for="cultivationType">{{
+          cultivationTypeNames[cultivationType] || cultivationType
+        }}</label
+        ><span class="checkmark"></span>
+      </div>
+    </div>
+    <div class="coldplant-container">
+      <p>Passar för vintersådd</p>
+      <div class="radios">
+        <input
+          class="radio-button"
+          type="radio"
+          id="allColdPlantingTypes"
+          value="allColdPlantingTypes"
+          v-model="chosenColdPlantingType"
+          name="coldPlantingType"
+        />
+        <label for="allColdPlantingTypes">Alla</label
+        ><span class="checkmark"></span>
+      </div>
+      <div class="radios" v-for="type in coldPlantingTypes" :key="type">
         <input
           class="radio-button"
           type="radio"
           :id="type"
           :value="type"
-          v-model="chosenCultivationType"
-          name="cultivationType"
+          v-model="chosenColdPlantingType"
+          name="coldPlantingType"
         />
-        <label :for="type">{{ cultivationTypeNames[type] || type }}</label
+        <label :for="type">{{ coldPlantingNames[type] || type }}</label
         ><span class="checkmark"></span>
       </div>
     </div>
@@ -88,105 +134,106 @@ watch(chosenCultivationType, (newValue) => {
 </template>
 
 <style scoped>
-section {
-  width: 30%;
-  padding: 0 1rem;
-  margin: 0 1rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  border-right: 2px solid var(--color-light-grey);
-}
-
-.searchfield-wrapper {
-  margin: 0 2rem 1rem 0;
-  .searchfield-name {
-    width: 100%;
-    padding: 1rem;
-    border-radius: 8px;
-    border: 1px solid var(--color-light-grey);
-    box-shadow: 0px 1px 3px -1px #ccc;
-
-    &::placeholder {
-      font-family: Fredoka, sans-serif;
-      font-weight: 400;
-      font-size: 1rem;
-    }
+  section {
+    width: 30%;
+    padding: 0 1rem;
+    margin: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    border-right: 2px solid var(--color-light-grey);
   }
-}
-.searchfield-name:focus {
-  border: 2px dashed var(--color-green);
-  outline: none;
-}
-.cultivation-container {
-  p {
-    margin: 0 0 1rem 0;
-  }
-  .radios {
-    padding-bottom: 0.6rem;
-    display: block;
-    position: relative;
-    cursor: pointer;
-    .radio-button {
-      display: none;
-    }
-    label {
-      font-size: 1.2rem;
-      margin: 1rem;
-      cursor: pointer;
-      font-size: 1.3rem;
-      padding: 0.2rem 1.5rem;
+
+  .searchfield-wrapper {
+    margin: 0 2rem 1rem 0;
+    .searchfield-name {
+      width: 100%;
+      padding: 1rem;
       border-radius: 8px;
-      input {
-        position: absolute;
-        opacity: 0;
-        cursor: pointer;
-        height: 0;
-        width: 0;
-      }
+      border: 1px solid var(--color-light-grey);
+      box-shadow: 0px 1px 3px -1px #ccc;
 
-      &:hover input ~ label {
+      &::placeholder {
+        font-family: Fredoka, sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+      }
+    }
+  }
+  .searchfield-name:focus {
+    border: 2px dashed var(--color-green);
+    outline: none;
+  }
+  .cultivation-container,
+  .coldplant-container {
+    p {
+      margin: 0 0 1rem 0;
+    }
+    .radios {
+      padding-bottom: 0.6rem;
+      display: block;
+      position: relative;
+      cursor: pointer;
+      .radio-button {
+        display: none;
+      }
+      label {
+        font-size: 1.2rem;
+        margin: 1rem;
+        cursor: pointer;
+        font-size: 1.3rem;
+        padding: 0.2rem 1.5rem;
+        border-radius: 8px;
+        input {
+          position: absolute;
+          opacity: 0;
+          cursor: pointer;
+          height: 0;
+          width: 0;
+        }
+
+        &:hover input ~ label {
+          background-color: #ccc;
+        }
+
+        input:checked ~ label {
+          background-color: var(--color-green);
+        }
+      }
+      .checkmark {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 25px;
+        width: 25px;
+        background-color: #eee;
+        border-radius: 50%;
+      }
+      &:hover input ~ .checkmark {
         background-color: #ccc;
       }
 
-      input:checked ~ label {
+      input:checked ~ .checkmark {
         background-color: var(--color-green);
       }
-    }
-    .checkmark {
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 25px;
-      width: 25px;
-      background-color: #eee;
-      border-radius: 50%;
-    }
-    &:hover input ~ .checkmark {
-      background-color: #ccc;
-    }
 
-    input:checked ~ .checkmark {
-      background-color: var(--color-green);
-    }
+      .checkmark:after {
+        content: '';
+        position: absolute;
+        display: none;
+      }
+      input:checked ~ .checkmark:after {
+        display: block;
+      }
 
-    .checkmark:after {
-      content: "";
-      position: absolute;
-      display: none;
-    }
-    input:checked ~ .checkmark:after {
-      display: block;
-    }
-
-    .checkmark:after {
-      top: 9px;
-      left: 9px;
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
-      background: white;
+      .checkmark:after {
+        top: 9px;
+        left: 9px;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: white;
+      }
     }
   }
-}
 </style>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,101 +1,117 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
-import { fetchFlowers } from "../../fetchFlowers";
-import FlowerList from "../components/FlowerList.vue";
-import QuerySection from "../components/QuerySection.vue";
-import Sorting from "../components/Sorting.vue";
+  import { ref, onMounted, computed } from 'vue'
+  import { fetchFlowers } from '../../fetchFlowers'
+  import FlowerList from '../components/FlowerList.vue'
+  import QuerySection from '../components/QuerySection.vue'
+  import Sorting from '../components/Sorting.vue'
 
-const flowers = ref([]);
-const searchQueryExists = ref(false);
-const categories = ref([]);
-const colors = ref([]);
-const cultivationTypes = ref([]);
-const searchQuery = ref("");
-const chosenCategory = ref([]);
-const chosenColor = ref([]);
-const chosenCultivationType = ref("all");
-const sortOrder = ref("");
-const sortCategory = ref("");
-const sortMonth = ref("");
+  const flowers = ref([])
+  const searchQueryExists = ref(false)
+  const categories = ref([])
+  const colors = ref([])
+  const cultivationTypes = ref([])
+  const coldPlantingTypes = ref([])
+  const searchQuery = ref('')
+  const chosenCategory = ref([])
+  const chosenColor = ref([])
+  const chosenCultivationType = ref('allCultivationTypes')
+  const chosenColdPlantingType = ref('allColdPlantingTypes')
+  const sortOrder = ref('')
+  const sortCategory = ref('')
+  const sortMonth = ref('')
 
-onMounted(async () => {
-  flowers.value = await fetchFlowers();
-  flowers.value = flowers.value.sort((a, b) => a.name.localeCompare(b.name));
-  categories.value = [
-    ...new Set(flowers.value.map((flower) => flower.category)),
-  ].sort();
-  cultivationTypes.value = [
-    ...new Set(flowers.value.map((flower) => flower.cultivationType)),
-  ];
-  colors.value = [
-    ...new Set(flowers.value.flatMap((flower) => flower.color)),
-  ].sort();
-  //NOTE TO SELF:
-  // Set är en datastruktur som tar bort dubbletter.
-  // Set skapar en unik uppsättning, men den måste konverteras tillbaka till en vanlig array. spread-operatorn (...) gör det = "sprider ut" värdena och skapar en ny array.
-  // flatMap() går igenom varje blomma (flower) i flowers.value och samlar ihop alla färger från varje blomma i en enda enkel lista. Vi använder flatMap här för att colors är en array i databasen. Hade det varit en enkel string, som category är, hade vi bara använt map().
-});
+  onMounted(async () => {
+    flowers.value = await fetchFlowers()
+    flowers.value = flowers.value.sort((a, b) => a.name.localeCompare(b.name))
+    categories.value = [
+      ...new Set(flowers.value.map((flower) => flower.category))
+    ].sort()
+    colors.value = [
+      ...new Set(flowers.value.flatMap((flower) => flower.color))
+    ].sort()
+    cultivationTypes.value = [
+      ...new Set(flowers.value.map((flower) => flower.cultivationType))
+    ]
+    coldPlantingTypes.value = [
+      ...new Set(flowers.value.map((flower) => flower.canBeColdPlanted))
+    ].sort((a, b) => {
+      if (a === 'Yes') return -1 // 'Yes' kommer före 'No'
+      if (b === 'Yes') return 1
+      return 0
+    })
+    //NOTE TO SELF:
+    // Set är en datastruktur som tar bort dubbletter.
+    // Set skapar en unik uppsättning, men den måste konverteras tillbaka till en vanlig array. spread-operatorn (...) gör det = "sprider ut" värdena och skapar en ny array.
+    // flatMap() går igenom varje blomma (flower) i flowers.value och samlar ihop alla färger från varje blomma i en enda enkel lista. Vi använder flatMap här för att colors är en array i databasen. Hade det varit en enkel string, som category är, hade vi bara använt map().
+  })
 
-const extractMonth = (dateString) => {
-  const splitString = dateString.split("/");
-  if (splitString.length === 2) {
-    const month = parseInt(splitString[1]);
-    return isNaN(month) ? 0 : month;
+  const extractMonth = (dateString) => {
+    const splitString = dateString.split('/')
+    if (splitString.length === 2) {
+      const month = parseInt(splitString[1])
+      return isNaN(month) ? 0 : month
+    }
+    return 0
   }
-  return 0;
-};
 
-//NOTE TO SELF: För att Övrigt-kategorin ska hamna sist måste jag göra en fallback då Ö har ett högre Unicode-värde än de andra bokstäverna i det svenska alfabetet. Sorteringsfunktionen i JavaScript använder sig av Unicode-värden för att jämföra strängar, så Ö kommer hamna först i en stigande sortering (A-Z). Den här funktionen säger att om någon av kategorierna är "Övrigt", placera den sist
-const customSort = (a, b) => {
-  if (a === "Övrigt") return 1; // "Övrigt" ska hamna efter andra kategorier
-  if (b === "Övrigt") return -1; // Andra kategorier ska hamna före "Övrigt"
-  return a.localeCompare(b, "sv");
-};
+  //NOTE TO SELF: För att Övrigt-kategorin ska hamna sist måste jag göra en fallback då Ö har ett högre Unicode-värde än de andra bokstäverna i det svenska alfabetet. Sorteringsfunktionen i JavaScript använder sig av Unicode-värden för att jämföra strängar, så Ö kommer hamna först i en stigande sortering (A-Z). Den här funktionen säger att om någon av kategorierna är "Övrigt", placera den sist
+  const customSort = (a, b) => {
+    if (a === 'Övrigt') return 1 // "Övrigt" ska hamna efter andra kategorier
+    if (b === 'Övrigt') return -1 // Andra kategorier ska hamna före "Övrigt"
+    return a.localeCompare(b, 'sv')
+  }
 
-const updateFilteredFlowers = computed(() => {
-  let result = flowers.value;
-  if (searchQuery.value) {
-    result = result.filter((flower) =>
-      flower.name.toLowerCase().includes(searchQuery.value.toLowerCase()),
-    );
-    searchQueryExists.value = true;
-  }
-  if (chosenCategory.value.length) {
-    result = result.filter((flower) =>
-      chosenCategory.value.includes(flower.category),
-    );
-    searchQueryExists.value = true;
-  }
-  if (chosenCultivationType.value !== "all") {
-    result = result.filter(
-      (flower) => flower.cultivationType === chosenCultivationType.value,
-    );
-    searchQueryExists.value = true;
-  }
-  if (chosenColor.value.length) {
-    result = result.filter((flower) =>
-      chosenColor.value.some((color) => flower.color.includes(color)),
-    );
-    //NOTE TO SELF: some() kollar om NÅGOT element i en array uppfyller ett villkor. Den returnerar true om den hittar minst ett element som matchar, annars false.
-    searchQueryExists.value = true;
-  }
-  if (sortOrder.value === "desc") {
-    result = result.sort((a, b) => b.name.localeCompare(a.name)); // Z-A
-  } else if (sortOrder.value === "asc") {
-    result.sort((a, b) => a.name.localeCompare(b.name));
-  }
-  if (sortCategory.value === "category") {
-    result.sort((a, b) => customSort(a.category, b.category));
-  }
-  if (sortMonth.value === "asc") {
-    return [...flowers.value].sort((a, b) => {
-      const monthA = extractMonth(a.sowingDate);
-      const monthB = extractMonth(b.sowingDate);
-      return monthA - monthB; // Jämför månaden numeriskt
-    });
-  }
-  return result;
-});
+  const updateFilteredFlowers = computed(() => {
+    let result = flowers.value
+    if (searchQuery.value) {
+      result = result.filter((flower) =>
+        flower.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+      )
+      searchQueryExists.value = true
+    }
+    if (chosenCategory.value.length) {
+      result = result.filter((flower) =>
+        chosenCategory.value.includes(flower.category)
+      )
+      searchQueryExists.value = true
+    }
+    if (chosenCultivationType.value !== 'allCultivationTypes') {
+      result = result.filter(
+        (flower) => flower.cultivationType === chosenCultivationType.value
+      )
+      searchQueryExists.value = true
+    }
+    if (chosenColdPlantingType.value !== 'allColdPlantingTypes') {
+      result = result.filter(
+        (flower) => flower.canBeColdPlanted === chosenColdPlantingType.value
+      )
+      searchQueryExists.value = true
+    }
+    if (chosenColor.value.length) {
+      result = result.filter((flower) =>
+        chosenColor.value.some((color) => flower.color.includes(color))
+      )
+      searchQueryExists.value = true
+
+      //NOTE TO SELF: some() kollar om NÅGOT element i en array uppfyller ett villkor. Den returnerar true om den hittar minst ett element som matchar, annars false.
+    }
+    if (sortOrder.value === 'desc') {
+      result = result.sort((a, b) => b.name.localeCompare(a.name)) // Z-A
+    } else if (sortOrder.value === 'asc') {
+      result.sort((a, b) => a.name.localeCompare(b.name))
+    }
+    if (sortCategory.value === 'category') {
+      result.sort((a, b) => customSort(a.category, b.category))
+    }
+    if (sortMonth.value === 'asc') {
+      return [...flowers.value].sort((a, b) => {
+        const monthA = extractMonth(a.sowingDate)
+        const monthB = extractMonth(b.sowingDate)
+        return monthA - monthB // Jämför månaden numeriskt
+      })
+    }
+    return result
+  })
 </script>
 
 <template>
@@ -110,9 +126,11 @@ const updateFilteredFlowers = computed(() => {
       v-model:chosenCategory="chosenCategory"
       v-model:chosenColor="chosenColor"
       v-model:chosenCultivationType="chosenCultivationType"
+      v-model:chosenColdPlantingType="chosenColdPlantingType"
       :categories="categories"
       :colors="colors"
       :cultivationTypes="cultivationTypes"
+      :coldPlantingTypes="coldPlantingTypes"
     />
     <FlowerList
       :flowers="updateFilteredFlowers"
@@ -127,11 +145,11 @@ const updateFilteredFlowers = computed(() => {
 </template>
 
 <style scoped>
-.main-content {
-  margin-top: 1rem;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
+  .main-content {
+    margin-top: 1rem;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
 </style>
